32ac6258808bb35d5000f7ceb90ad9aa7e8c7507
Ali Mahani <ali.a.mahani@zoho.com>
2025-04-10 14:52:31 -0600

    chore: clean up code and migrate tests to the test directory.

---
+++ ml_force/utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def z_transform(signal: np.ndarray):
+    return (signal - signal.mean(axis=0)) / signal.std(axis=0)
+
+
+def minmax_transform(signal, zero_mean: bool = False):
+    min = signal.min(axis=0)
+    max = signal.max(axis=0)
+    minmaxed = (signal - min) / (max - min)
+    if zero_mean:
+        minmaxed -= minmaxed.mean(axis=0)
+    return minmaxed
+
+
