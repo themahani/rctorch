1f643a5c3003bac309119c441c2ad37276ce5c71
Ali Mahani <ali.a.mahani@zoho.com>
2025-05-24 14:01:26 -0600

    feat(models): add basic parent class for all models

---
+++ ml_force/models/base.py
@@ -0,0 +1,60 @@
+import torch
+from torch import Tensor
+from torch.nn import (
+    Module,
+    Parameter,
+)
+
+
+class SNNBase(Module):
+
+    __constants__ = ["n_hidden"]
+    n_hidden: int
+
+    __variables__ = ["mem", "rate"]
+    mem: torch.Tensor
+    rate: torch.Tensor
+
+    def __init__(
+        self,
+        n_hidden: int,
+        dt: float,
+        device: torch.DeviceObjType = None,
+        dtype: torch.dtype = None,
+    ) -> None:
+        super().__init__()
+        factory_kwargs = {"device": device, "dtype": dtype}
+        self.n_hidden = n_hidden
+        self.dt = dt
+
+        # Neuron Variables
+        self.mem = torch.zeros(size=(n_hidden, 1), **factory_kwargs)
+
+    def _init_mem(self) -> None:
+        self.mem = torch.zeros(size=(self.n_hidden, 1))  # NOTE: Might have to add factory_kwargs
+
+    def mem_dot(self, input_: Tensor) -> Tensor:
+        r"""Calculate the first time derivative of the membrane potential.
+        :param input_: Input current
+        :type input_: torch.Tensor
+
+        :returns:
+            - mem_dot_ (torch.Tensor): The time derivative of :param:`self.mem`
+        :rtype: torch.Tensor
+        """
+        return -self.mem + input_
+
+    def forward(self, input_: Tensor) -> Tensor:
+        r"""
+        :param input_: Input tensor for the current time step.
+        :type input_: torch.Tensor, shape (`nt`, )
+
+        :returns:
+            - output (torch.Tensor): Updated membrane potential
+        :rtype: (torch.Tensor)
+        """
+        if self.mem is None:
+            self._init_mem()
+        # Example update for `mem`
+        self.mem += self.dt * self.mem_dot(input_)
+        return self.mem
